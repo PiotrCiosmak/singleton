8f943479e1bb5c147a3cb8019a30bf5144316727
PiotrCiosmak <piotrciosmak2001@gmail.com>
2022-12-30 19:26:26 +0100

    practical use of singleton

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("If you see the same value, then singleton was reused");
+        System.out.println("If you see different values, then 2 singletons were created");
+        System.out.println("RESULT:");
+        Singleton firstSingleton = Singleton.getInstance("HI");
+        Singleton secondSingleton = Singleton.getInstance("HELLO");
+        System.out.println(firstSingleton.getValue());
+        System.out.println(secondSingleton.getValue());
     }
 }
